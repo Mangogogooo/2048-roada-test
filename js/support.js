eaa8931c7fb6f9123dfcec90e387c015ce0a6613
BusyMangoGoGoGo <1049383447@qq.com>
2021-02-09 00:06:35 +0800

    nitial commit

--- js/support.js
+++ js/support.js
@@ -19,17 +19,17 @@ function getLeft(i,j){
 
 function getNumberBackgroundColor(number){
     switch(number){
-        case 2:return "#eee4da";break;
-        case 4:return "#ede0c8";break;
-        case 8:return "#f2b179";break;
-        case 16:return "#f59563";break;
-        case 32:return "#f67c5f";break;
-        case 64:return "#f65e3b";break;
-        case 128:return "#edcf72";break;
-        case 256:return "#edcc61";break;
-        case 512:return "#9c0";break;
-        case 1024:return "#33b5e5";break;
-        case 2048:return "#09c";break;
+        case 2:return "url(images/2.jpg)";break;
+        case 4:return "url(images/4.jpg)";break;
+        case 8:return "url(images/8.jpg)";break;
+        case 16:return "url(images/16.jpg)";break;
+        case 32:return "url(images/32.jpg)";break;
+        case 64:return "url(images/64.jpg)";break;
+        case 128:return "url(images/128.jpg)";break;
+        case 256:return "url(images/256.jpg)";break;
+        case 512:return "url(images/512.jpg)";break;
+        case 1024:return "url(images/1024.jpg)";break;
+        case 2048:return "url(images/2048.jpg)";break;
         case 4096:return "#a6c";break;
         case 8192:return "#93c";break;
     }
